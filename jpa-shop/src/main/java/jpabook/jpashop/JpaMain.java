834f95c99a822865f05a68b36245c247e41057fd
pok3mon025 <pok3mon025@gmail.com>
2023-04-15 19:50:42 +0900

    initial commit

---
+++ jpa-shop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            //객체지향스럽지 않은 코드
+            Order order = em.find(Order.class, 1L);
+            Long memberId = order.getMemberId();
+
+            Member member = em.find(Member.class, memberId);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+}
